f47152ba479e6b3e588310f06aca61b71217021f
Morgan Lin <morganplin@gmail.com>
2016-01-25 12:19:18 -0800

    added bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = Promise.promisifyAll(require('../node_modules/bcrypt'));
+var Promise = require('bluebird');
+
 
 var userSchema = Schema({
       name                : { first : String,
@@ -18,5 +21,26 @@ var userSchema = Schema({
 
 })
 
+userSchema.pre('save',function (next){
+    var user = this;
 
-module.exports = mongoose.model('User', userSchema)
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    return bcrypt.genSaltAsync(10).then(function(result) {
+
+        return bcrypt.hashAsync(user.password, result).then(function(hash){
+            console.log("hash: " + hash);
+            user.password = hash;
+            next();
+        });
+    })
+});
+
+userSchema.methods.comparePasswordAsync = function(candidatePassword) {
+    return bcrypt.compareAsync(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
+module.exports = User;
+
