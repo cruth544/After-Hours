3b17988349c6bd5846ffb2ad827f643863e56ede
Chad Rutherford <c.ruth544@gmail.com>
2016-01-26 09:35:01 -0800

    just removed yelp code

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -16,34 +16,7 @@ module.exports = {
                     "longitude": -118.268692
                   }
     io.emit('drop-pin', {location: location})
-  //******************************************************************\\
- //COMMENTED OUT BECAUSE YELP WILL LOCK YOU OUT AFTER TOO MANY REQUESTS\\
-//**********************************************************************\\
-    // var businessesJson = {}
-    // var responsesCompleted = 0
-    // var yelp = new Yelp({
-    //   consumer_key: 'ppGF7cs331hgnbdAMFtrKQ',
-    //   consumer_secret: 'vZIA3bJyHdn9q9JLLgLeY6c35eE',
-    //   token: '8pSTKEbNQJ7P8zx8ECZdIUDknncrjPLq',
-    //   token_secret: 'Z2t6RPX8FOlA43xpFmWppg8J_hI'
-    // })
-
-    // yelp.search({ term: 'happy hour', location: 'Los Angeles'})
-    //   .then(function (data) {
-        res.render('index', {/*data: data, */curr_user: null})
-
-      //   yelpParse(data, businessesJson, function () {
-      //     responsesCompleted++
-      //     console.log(responsesCompleted)
-      //     if (responsesCompleted === data.businesses.length) {
-      //       fs.writeFile('output.json', JSON.stringify(businessesJson, null, 4), function(err){
-      //         console.log('File successfully written! - Check your project directory for the output.json file');
-      //       })
-      //     }
-      //   })
-      // }).catch(function (err) {
-      //   console.log(err)
-      // })
+    res.render('index', {/*data: data, */curr_user: null})
   },
 
   all: function (req, res, next) {
