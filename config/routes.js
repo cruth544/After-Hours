015e2c03008ce08a032a60a12c6d00315b3f6410
Chad Rutherford <c.ruth544@gmail.com>
2016-01-26 02:42:44 -0800

    removed fb oauth and search bar feature

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var router  = new express.Router()
 var usersController = require('../controllers/users')
-var passport = require('passport')
-require("./passport")(passport)
 var restaurantsController = require('../controllers/restaurants')
 
 router.route('/')
@@ -16,26 +14,6 @@ router.route('/signUp')
 
 router.route('/logout')
   .get(usersController.logout)
-
-router.route('/auth/facebook')
-  .get(passport.authenticate('facebook', {scope: 'email'}));
-
-// app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email'} ));
-
-router.route('/auth/facebook/callback')
-  .get(passport.authenticate('facebook', {
-    successRedirect: '/',
-    failureRedirect: '/'
-  }));
-
-// app.get('/auth/facebook/callback',
-//   passport.authenticate('facebook', {
-//     successRedirect: '/',
-//     failureRedirect: '/'
-//   })
-// );
-
-
 
 // router.get('/welcome', usersController.index);
 module.exports = router;
