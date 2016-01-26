631d267e8d6fbf76b1778e3f64aaa7820502ce66
Stacey Sugiono <staceysugiono@gmail.com>
2016-01-25 22:00:42 -0800

    added create method

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -14,6 +14,18 @@ module.exports = {
       Restaurant.findOne({ name: String(req.params.name)}, function (err, restaurant) {
         res.render('restaurants/show', { restaurant: restaurant })
       })
+    },
 
+    create: function (req, res, next) {
+      var newRestaurant = new Restaurant()
+      var keys = Object.keys(req.body)
+      keys.forEach(function (key){
+        newRestaurant[key] = req.body[key]
+      })
+
+      newRestaurant.save(function (err) {
+        if (err) console.log(err)
+        else res.send('Restaurant created!')
+      })
     }
 }
