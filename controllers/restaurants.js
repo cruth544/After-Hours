e6016d4781d49bcfe789fcc873b723b55885f5f3
Chad Rutherford <c.ruth544@gmail.com>
2016-01-26 01:05:55 -0800

    commented out yelp requests due to getting locked out

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -32,145 +32,155 @@ var cheerio     = require('cheerio')
 var async       = require('async')
 var fs          = require('fs')
 var request     = require('request')
-var map         = require('../public/js/google-maps')
 
 module.exports = {
+  //******************************************************************\\
+ //COMMENTED OUT BECAUSE YELP WILL LOCK YOU OUT AFTER TOO MANY REQUESTS\\
+//**********************************************************************\\
   index: function (req, res, next) {
-    var map = cheerio.load('#map')
-    // console.log("from restaurant.js")
-    // console.log(map)
-    var businessesJson = {}
-    var responsesCompleted = 0
-    var yelp = new Yelp({
-      consumer_key: 'ppGF7cs331hgnbdAMFtrKQ',
-      consumer_secret: 'vZIA3bJyHdn9q9JLLgLeY6c35eE',
-      token: '8pSTKEbNQJ7P8zx8ECZdIUDknncrjPLq',
-      token_secret: 'Z2t6RPX8FOlA43xpFmWppg8J_hI'
-    })
+    // var businessesJson = {}
+    // var responsesCompleted = 0
+    // var yelp = new Yelp({
+    //   consumer_key: 'ppGF7cs331hgnbdAMFtrKQ',
+    //   consumer_secret: 'vZIA3bJyHdn9q9JLLgLeY6c35eE',
+    //   token: '8pSTKEbNQJ7P8zx8ECZdIUDknncrjPLq',
+    //   token_secret: 'Z2t6RPX8FOlA43xpFmWppg8J_hI'
+    // })
 
-    yelp.search({ term: 'happy hour', location: 'Los Angeles'})
-      .then(function (data) {
+    // yelp.search({ term: 'happy hour', location: 'Los Angeles'})
+    //   .then(function (data) {
         res.render('index', {data: data, curr_user: null})
 
-        // yelpParse(data, businessesJson, function () {
-        //   responsesCompleted++
-        //   console.log(responsesCompleted)
-        //   if (responsesCompleted === data.businesses.length) {
-        //     fs.writeFile('output.json', JSON.stringify(businessesJson, null, 4), function(err){
-        //       console.log('File successfully written! - Check your project directory for the output.json file');
-        //     })
-        //   }
-        // })
-      }).catch(function (err) {
-        console.log(err)
-      })
+      //   yelpParse(data, businessesJson, function () {
+      //     responsesCompleted++
+      //     console.log(responsesCompleted)
+      //     if (responsesCompleted === data.businesses.length) {
+      //       fs.writeFile('output.json', JSON.stringify(businessesJson, null, 4), function(err){
+      //         console.log('File successfully written! - Check your project directory for the output.json file');
+      //       })
+      //     }
+      //   })
+      // }).catch(function (err) {
+      //   console.log(err)
+      // })
   }
 }
 
-function yelpParse (data, businessJson, complete) {
-  var businesses = data.businesses
-  var businessCount = 0
-  var promiseArray = []
-  async.whilst(
-    // Condition to check every loop
-    function () {
-      return businessCount < businesses.length
-    },
-    // function to call every iteration
-    function (callback) {
-      var restaurant = businesses[businessCount]
-      businessJson[restaurant.name] = {}
-      businessJson[restaurant.name].location = restaurant.location.coordinate
-      getReviewCount(restaurant.name, restaurant.url, businessJson, function () {
-        complete()
-      })
 
-      // increment counter
-      businessCount++
-      // you have to call the callback to go to next
-      // iteration of the loop
-      callback()
-    },
-    // Stopped loop
-    function (err) {
-    })
-}
+  //******************************************************************\\
+ //COMMENTED OUT BECAUSE YELP WILL LOCK YOU OUT AFTER TOO MANY REQUESTS\\
+//**********************************************************************\\
+  //******************************************************************\\
+ //COMMENTED OUT BECAUSE YELP WILL LOCK YOU OUT AFTER TOO MANY REQUESTS\\
+//**********************************************************************\\
+  //******************************************************************\\
+ //COMMENTED OUT BECAUSE YELP WILL LOCK YOU OUT AFTER TOO MANY REQUESTS\\
+//**********************************************************************\\
 
-function getReviewCount (name, url, businessJson, complete) {
-  url = url.split('?')[0]
-  var yelpSearchUrl = url
-  yelpSearchUrl += '?q=happy%20hour'
-  request({url: yelpSearchUrl}, function (err, res, body) {
-    if (!err) {
-      // user cheerio to load html webpage
-      var $ = cheerio.load(body)
-      // make regex to get number
-      var reviewCountRegEx = /\d+\b/
-      // grab node where the review count is
-      var reviewCount = $('.feed_search-results').first().text()
-      // use regex to extract number from node
-      reviewCount = reviewCount.match(reviewCountRegEx)[0]
-      extractHappyHourTime(name, businessJson, reviewCount, url, function () {
-        complete()
-      })
-      // extractHappyHourTime(name, reviewCount, url, function (object) {
-      //   complete(object)
-      // })
-    } else {
-      console.log(err)
-    }
-  })
-}
+// function yelpParse (data, businessJson, complete) {
+//   var businesses = data.businesses
+//   var businessCount = 0
+//   var promiseArray = []
+//   async.whilst(
+//     // Condition to check every loop
+//     function () {
+//       return businessCount < businesses.length
+//     },
+//     // function to call every iteration
+//     function (callback) {
+//       var restaurant = businesses[businessCount]
+//       businessJson[restaurant.name] = {}
+//       businessJson[restaurant.name].location = restaurant.location.coordinate
+//       getReviewCount(restaurant.name, restaurant.url, businessJson, function () {
+//         complete()
+//       })
 
-function extractHappyHourTime (name, businessJson, reviewCount, url, complete) {
-  var counter = 0
-  var restaurantJson = {}
-  async.whilst(
-    function () {
-      // console.log("Counter: " + counter)
-      // console.log("is less than")
-      // console.log("ReviewCount: " + reviewCount)
-      // console.log(counter < reviewCount)
-      return counter < reviewCount
-    },
-    function (callback) {
+//       // increment counter
+//       businessCount++
+//       // you have to call the callback to go to next
+//       // iteration of the loop
+//       callback()
+//     },
+//     // Stopped loop
+//     function (err) {
+//     })
+// }
 
-      // console.log(url)
-      url += '?start=' + counter
-      url += '&q=happy%20hour'
-      // console.log(url)
-      request({url: url}, function (err, res, body) {
-        if (!err) {
-          var $ = cheerio.load(body)
-          var reviews = $('.review-content')
-          var happyHoursRegEx = /(from)?\s1?\d?:?\d{0,2}\s?([AaPp]\.?[mM]\.?)?\s?(is|to|through|until|and|-)\s?\d{1,2}:?\d{0,2}\s?([AaPp]\.?[mM]\.?)?/
-          async.forEachOf(reviews, function (item, key, forEachCallback) {
-            var review = $(item).children().last().text()
-            if (happyHoursRegEx.test(review)) {
-              review = review.match(happyHoursRegEx)
-              var jsonKey = Number(key) + counter
-              restaurantJson[jsonKey] = review
-              forEachCallback()
-            }
-          })
-          counter += 20
-          // console.log("incrementing... " + counter)
-          callback()
-        }
-      })
-    },
-    function (err) {
-      // console.log(restaurantJson)
-      // console.log("stopped extractHappyHourTime")
-      console.log(name)
-      businessJson[name].reviews = restaurantJson
-      complete()
-      // fs.writeFile('output.json', JSON.stringify(allRestaurants, null, 4), function(err){
-      //         console.log('File successfully written! - Check your project directory for the output.json file');
-      //       })
-    }
-  )
-}
+// function getReviewCount (name, url, businessJson, complete) {
+//   url = url.split('?')[0]
+//   var yelpSearchUrl = url
+//   yelpSearchUrl += '?q=happy%20hour'
+//   request({url: yelpSearchUrl}, function (err, res, body) {
+//     if (!err) {
+//       // user cheerio to load html webpage
+//       var $ = cheerio.load(body)
+//       // make regex to get number
+//       var reviewCountRegEx = /\d+\b/
+//       // grab node where the review count is
+//       var reviewCount = $('.feed_search-results').first().text()
+//       // use regex to extract number from node
+//       reviewCount = reviewCount.match(reviewCountRegEx)[0]
+//       extractHappyHourTime(name, businessJson, reviewCount, url, function () {
+//         complete()
+//       })
+//       // extractHappyHourTime(name, reviewCount, url, function (object) {
+//       //   complete(object)
+//       // })
+//     } else {
+//       console.log(err)
+//     }
+//   })
+// }
+
+// function extractHappyHourTime (name, businessJson, reviewCount, url, complete) {
+//   var counter = 0
+//   var restaurantJson = {}
+//   async.whilst(
+//     function () {
+//       // console.log("Counter: " + counter)
+//       // console.log("is less than")
+//       // console.log("ReviewCount: " + reviewCount)
+//       // console.log(counter < reviewCount)
+//       return counter < reviewCount
+//     },
+//     function (callback) {
+
+//       // console.log(url)
+//       url += '?start=' + counter
+//       url += '&q=happy%20hour'
+//       // console.log(url)
+//       request({url: url}, function (err, res, body) {
+//         if (!err) {
+//           var $ = cheerio.load(body)
+//           var reviews = $('.review-content')
+//           var happyHoursRegEx = /(from)?\s1?\d?:?\d{0,2}\s?([AaPp]\.?[mM]\.?)?\s?(is|to|through|until|and|-)\s?\d{1,2}:?\d{0,2}\s?([AaPp]\.?[mM]\.?)?/
+//           async.forEachOf(reviews, function (item, key, forEachCallback) {
+//             var review = $(item).children().last().text()
+//             if (happyHoursRegEx.test(review)) {
+//               review = review.match(happyHoursRegEx)
+//               var jsonKey = Number(key) + counter
+//               restaurantJson[jsonKey] = review
+//               forEachCallback()
+//             }
+//           })
+//           counter += 20
+//           // console.log("incrementing... " + counter)
+//           callback()
+//         }
+//       })
+//     },
+//     function (err) {
+//       // console.log(restaurantJson)
+//       // console.log("stopped extractHappyHourTime")
+//       console.log(name)
+//       businessJson[name].reviews = restaurantJson
+//       complete()
+//       // fs.writeFile('output.json', JSON.stringify(allRestaurants, null, 4), function(err){
+//       //         console.log('File successfully written! - Check your project directory for the output.json file');
+//       //       })
+//     }
+//   )
+// }
 
 
 
