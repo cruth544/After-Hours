18af6cbd69ab5c352a06ac509d8591fdb4096393
Michael Washburn <washburn.michael@gmail.com>
2016-01-26 17:58:46 -0800

    added MONGOLAB URI at credentials.js

--- db/credentials.js
+++ db/credentials.js
@@ -4,7 +4,7 @@ module.exports = {
             conn: process.env.DEV_CONN_STRING
         },
         prod: {
-            conn: process.env.PROD_CONN_STRING
+            conn: 'mongodb://heroku_h1907377:m8hp6hevivda2niaqbi1bu6gi1@ds051665.mongolab.com:51665/heroku_h1907377'
         },
         options:{
             server: {
