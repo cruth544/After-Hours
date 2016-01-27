d9f6d16c05ea4d7b435281c308f862f3f037c75d
Michael Washburn <washburn.michael@gmail.com>
2016-01-26 18:21:36 -0800

    updated mongolab info to try to deploy on heroku

--- db/credentials.js
+++ db/credentials.js
@@ -4,7 +4,7 @@ module.exports = {
             conn: process.env.DEV_CONN_STRING
         },
         prod: {
-            conn: 'mongodb://heroku_h1907377:m8hp6hevivda2niaqbi1bu6gi1@ds051665.mongolab.com:51665/heroku_h1907377'
+            conn: process.env.PROD_CONN_STRING
         },
         options:{
             server: {
