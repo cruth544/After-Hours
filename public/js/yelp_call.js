130c219f2f0bc2b04268f7a996196a10be24d094
Morgan Lin <morganplin@gmail.com>
2016-01-27 14:18:10 -0800

    merge conflict

---
+++ public/js/yelp_call.js
@@ -0,0 +1,165 @@
+function getCityByPosition (position) {
+  var geocoder = new google.maps.Geocoder()
+  geocoder.geocode({ location: position }, function (results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].types[0] === 'postal_code') {
+          var generalLocationArray = results[i].address_components
+          var zipCodeRegEx = /\d{5}/
+          for (var j = 0; j < generalLocationArray.length; j++) {
+            if (zipCodeRegEx.test(generalLocationArray[j].long_name)) {
+              ajaxCall(generalLocationArray[j].long_name, position)
+              break
+            }
+          }
+          break
+        }
+      }
+    } else {
+      alert('Geocoder failed due to: ' + status)
+    }
+  })
+}
+
+getMyPosition(null, function (position) {
+  getCityByPosition(position)
+})
+
+function ajaxCall (zip, geo) {
+  var geoString = geo.lat + "," + geo.lng
+
+  $.ajax({
+    url: '/restaurants/getAll',
+    type: 'GET',
+    data: {zipCode: zip, geoLocation: geoString}
+  })
+  .done(function(data) {
+    var restaurantArray = sortByDistance(geo, addObjectsToArray(data))
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var labelIndex = 0
+    for (var i = 0; i < restaurantArray.length; i++) {
+      restaurantArray[i]
+      function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null)
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE)
+        }
+      }
+      var marker = new google.maps.Marker({
+        position: restaurantArray[i].contact.coordinates,
+        label: labels[labelIndex++ % labels.length],
+        map: map,
+        animation: google.maps.Animation.DROP
+      })
+    }
+  })
+  .fail(function() {
+    console.log("error")
+  })
+  .always(function() {
+    console.log("complete")
+  })
+}
+
+function sortByDistance (position, restaurantArray) {
+  function distance (coordinates) {
+    if (coordinates) {
+      return (coordinates.lat - position.lat) * (coordinates.lat - position.lat) +
+      (coordinates.lng - position.lng) * (coordinates.lng - position.lng)
+    } else {
+      return -1
+    }
+  }
+  restaurantArray.sort(function (location1, location2) {
+    return distance(location1.contact.coordinates) - distance(location2.contact.coordinates)
+  })
+  // console.log(restaurantArray)
+  return restaurantArray
+}
+
+function addObjectsToArray (object) {
+  var array = []
+  Object.keys(object).map(function(val) {
+    object[val].name = val
+    array.push(object[val])
+  })
+  return array
+}
+
+function getMyPosition (defaultPosition, completeCallback) {
+  var pos = {}
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      if (completeCallback) {
+        completeCallback(pos)
+      }
+      return pos
+    })
+  } else {
+    if (defaultPosition) {
+      pos = defaultPosition
+    } else {
+      pos = {
+        lat: 34.0309344,
+        lng: -118.2688299
+      }
+    }
+    return pos
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
