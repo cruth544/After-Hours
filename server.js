5e7ba6023977af7245291e87a5e582fa8a46f8ed
Michael Washburn <washburn.michael@gmail.com>
2016-01-28 10:44:44 -0800

    updated code for heroku. FAILED

--- server.js
+++ server.js
@@ -79,5 +79,5 @@ switch(app.get('env')){
 //   })
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);});
-console.log("Server starting...go to localhost:3000")
+// console.log("Server starting...go to localhost:3000")
 
