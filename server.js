cef3844d240a59f0f9815f64fa0b637da7af7deb
Michael Washburn <washburn.michael@gmail.com>
2016-01-27 19:07:14 -0800

    added exports = module.exports = session;

--- server.js
+++ server.js
@@ -69,3 +69,4 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 console.log("Server starting...go to localhost:3000")
+exports = module.exports = session;
