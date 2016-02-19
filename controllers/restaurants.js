b2f4a25196c42ac666159454890591be3af654c4
Chad Rutherford <c.ruth544@gmail.com>
2016-02-19 15:16:38 -0800

    limit uped to 10

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -382,7 +382,7 @@ module.exports = {
     var searchParams = {
       term: 'happy hour',
       location: req.query.location,
-      limit: '1',
+      limit: '10',
       offset: req.query.offset,
       sort: '0'
     }
